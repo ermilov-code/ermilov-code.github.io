de3e7457172e735a3ed2d4b79e9df5f7cdea53f5
paratrooper-94 <ermilov1994@yandex.ru>
2021-03-02 11:48:54 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 // ============================================================
 
+
+window.onload = function () {
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+        return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    lax.addElements('.h2-heading', {
+        scrollY: {
+            translateX: [
+                ["elInY", "elCenterY", "elOutY"],
+                ['screenWidth', 'screenHeight/10', 0],
+            ]
+        }
+    })
+}
+
 // бургер меню
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
