7c0be8d11689ed9849c0d07e6c54c5d1ad5d9cf8
paratrooper-94 <ermilov1994@yandex.ru>
2021-04-22 16:08:39 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,69 +1,40 @@
 // ============================================================
 
 
-$('.part').click(
-    function () {
-        $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn(500);
-    }
-)
 
-$('.description').click(
-    function () {
-        $('.description').fadeOut(500);
-    }
-)
+// burger-menu (jquery) =================
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__button', '.burger-menu__lines');
+    let links = menu.find('.burger-menu__link');
+    let overlay = menu.find('.burger-menu__overlay');
 
-$('.button-cities').click(
-    function () {
-        $('.description').html($(this).attr('description-data'));
-        $('.description').fadeIn(500);
-    }
-)
-
-
-
-
-
-$(function () {
-    $('#input-city-search').on('change', function () {
-        var val = $('#input-city-search').val();
-
-        var data_id = $('#accessible-cities').find('option[value="' + val + '"]').data('id');
-        alert(data_id);
-
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
     });
 
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
 
-});
+    function toggleMenu() {
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overlow', 'hidden');
+        } else {
+            $('body').css('overlow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
+// // / burger-menu (jquery) =================
 
 
-
-// ИНТЕРАКТИВНАЯ карта
-
-
-
-
-// бургер меню
-$(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
-
-$(document).ready(function () {
-    $('.nav_item_burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
-
-// плавный якорь
-// Найти все ссылки начинающиеся на #
+// smooth scrolling =================
 const anchors = document.querySelectorAll('a[href^="#"]')
-
-// Цикл по всем ссылкам
+// Loop through all links
 for (let anchor of anchors) {
     anchor.addEventListener("click", function (e) {
         e.preventDefault() // Предотвратить стандартное поведение ссылок
@@ -76,89 +47,30 @@ for (let anchor of anchors) {
         })
     })
 }
+// smooth scrolling =================
 
 
 
 
 
 
-// https://computy.ru/blog/stilizacija-select-kak-budto-jeto-2019/
-$('.select').each(function () {
-    const _this = $(this),
-        selectOption = _this.find('option'),
-        selectOptionLength = selectOption.length,
-        selectedOption = selectOption.filter(':selected'),
-        duration = 450; // длительность анимации 
-
-    _this.hide();
-    _this.wrap('<div class="select"></div>');
-    $('<div>', {
-        class: 'new-select',
-        text: _this.children('option:disabled').text()
-    }).insertAfter(_this);
-
-    const selectHead = _this.next('.new-select');
-    $('<div>', {
-        class: 'new-select__list'
-    }).insertAfter(selectHead);
-
-    const selectList = selectHead.next('.new-select__list');
-    for (let i = 1; i < selectOptionLength; i++) {
-        $('<div>', {
-                class: 'new-select__item',
-                html: $('<span>', {
-                    text: selectOption.eq(i).text()
-                })
-            })
-            .attr('data-value', selectOption.eq(i).val())
-            .appendTo(selectList);
-    }
-
-    const selectItem = selectList.find('.new-select__item');
-    selectList.slideUp(0);
-    selectHead.on('click', function () {
-        if (!$(this).hasClass('on')) {
-            $(this).addClass('on');
-            selectList.slideDown(duration);
-
-            selectItem.on('click', function () {
-                let chooseItem = $(this).data('value');
-
-                $('select').val(chooseItem).attr('selected', 'selected');
-                selectHead.text($(this).find('span').text());
-
-                selectList.slideUp(duration);
-                selectHead.removeClass('on');
-            });
-
-        } else {
-            $(this).removeClass('on');
-            selectList.slideUp(duration);
-        }
-    });
-});
-// https://computy.ru/blog/stilizacija-select-kak-budto-jeto-2019/
 
 
 
-// КНОПКА ВВЕРХ НА САЙТЕ
-
-let btnUp = $('#btnUp'); //создаем переменную для кнопки
-
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 500) { //отслеживаем высоту от верха страницы в 100px
-        btnUp.addClass('show'); // присваиваем кнопке класс show для управления видимостью
-    } else {
-        btnUp.removeClass('show'); // если меньше 500px от верха страницы убираем класс show
-    }
-});
-
-btnUp.on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: 0
-    }, '500'); //при клике на кнопку плавно прокручиваем до верха body
-});
 
 
-// КНОПКА ВВЕРХ НА САЙТЕ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============================================================
