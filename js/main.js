3504caf5bfb011bc716a9faea4830da3380e1c58
paratrooper-94 <ermilov1994@yandex.ru>
2021-07-19 23:20:53 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,123 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
+];
+
+const renderProduct = (title, price) => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};  
+
+// в теле функции - с помощью .map - для каждого нашего объекта 
+// на выходе получаем новый массив productsList - содержащий элементы уже с версткой 
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item.title, item.price));
+    // item - каждый элемент нашего массива 
+    console.log(productsList);
+    // обращаемся к тегу div с классом .products - и в верстке этого элемента вставляем наш массив товаров с версткой 
+    document.querySelector('.products').innerHTML = productsList;
+};
+
+// на вход даем ей наш массив объектов 
+renderPage(products);
+
+
+
+
+
+
+// Меню бургер на JavaScript
+
+"use strict"
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
+
+} else {
+	document.body.classList.add('_pc');
+}
+
+// Меню бургер 
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+
+
+// Меню бургер на JavaScript
