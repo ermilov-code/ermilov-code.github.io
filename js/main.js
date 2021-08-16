311dd849b490a355e23e4aabfa65da623a4e54b2
paratrooper-94 <ermilov1994@yandex.ru>
2021-08-16 22:21:12 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,32 +1,55 @@
+// ======================================================
+
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+
+    {id: 1, title: 'Apple iPhone XR, 128 ГБ, белый', price: 52990, img: "img/1-AppleiPhoneXR,128ГБ,белый,52990₽.png"},
+
+	{id: 4, title: 'Apple iPhone XR, 128 ГБ, (PRODUCT)RED', price: 52990, img: "img/4-AppleiPhoneXR,128ГБ,(PRODUCT)RED,52990₽.png"},
+
+	{id: 2, title: 'Apple iPhone 11, 256 ГБ, фиолетовый', price: 67990, img: "img/2-AppleiPhone11,256ГБ,фиолетовый,67990₽.png"},
+
+	{id: 6, title: 'Apple iPad 10,2 Wi-Fi 32 ГБ, золотой', price: 29990, img: "img/6-AppleiPad10,2Wi-Fi32ГБ,золотой,29990₽.png"},
+
+	{id: 3, title: 'Apple iPad Pro (2021) Wi-Fi+Cellular 2 ТБ', price: 219990, img: "img/3-AppleiPadPro(2021)12,9Wi-Fi+Cellular2ТБ,219990₽.png"},
+
+	{id: 5, title: 'Apple Watch Series 6, 44 мм', price: 39490, img: "img/5-AppleWatchSeries6,44мм,39490₽.png"},
+
+	{id: 7, title: 'Apple MacBook Pro 13 (M1, 2020) 8ГБ, 512 ГБ SSD', price: 137990, img: "img/7-AppleMacBookPro13(M1,2020)8ГБ,512ГБSSD,137990₽.png"},
+
+	{id: 8, title: 'Apple iMac 24 Retina 4,5, M1, 8 ГБ, 256 ГБ SSD', price: 129990, img: "img/8-AppleiMac24Retina4,5K,M1(8CCPU,7CGPU),8ГБ,256ГБSSD,129990₽.png"},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+const renderProduct = (img, title, price) => {
+    return `<div class="products__item">
+				<h3 class="products__name">${title}</h3>
+                <img class="products__img" src="${img}" alt="${title}">
+                <div class="products__description">
+                    <p class="products__price">${price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+                <!-- /.products__description -->
             </div>`
 };  
 
 // в теле функции - с помощью .map - для каждого нашего объекта 
 // на выходе получаем новый массив productsList - содержащий элементы уже с версткой 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item.img, item.title, item.price));
     // item - каждый элемент нашего массива 
     console.log(productsList);
     // обращаемся к тегу div с классом .products - и в верстке этого элемента вставляем наш массив товаров с версткой 
-    document.querySelector('.products').innerHTML = productsList;
+
+	// УДАЛЯЕМ ЗАПЯТЫЕ
+	// .join(''); - метод для преобразования массива в стоку (НО! по умолчанию преобразует в строку с запятой) - но мы в качестве нашего разделителя пишем просто пустые кавычки;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
+
+
 
 // на вход даем ей наш массив объектов 
 renderPage(products);
 
-
+// ======================================================
 
 
 
