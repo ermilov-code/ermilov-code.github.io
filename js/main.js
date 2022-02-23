f16aa78ae69033c971901f1eb51755b8030cd19e
paratrooper-94 <ermilov1994@yandex.ru>
2022-02-23 18:34:07 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,35 +2,94 @@
 
 
 
-// burger-menu (jquery) =================
-function burgerMenu(selector) {
-    let menu = $(selector);
-    let button = menu.find('.burger-menu__button', '.burger-menu__lines');
-    let links = menu.find('.burger-menu__link');
-    let overlay = menu.find('.burger-menu__overlay');
+// ============= КОД ДЛЯ БУРГЕР МЕНЮ =============
 
-    button.on('click', (e) => {
-        e.preventDefault();
-        toggleMenu();
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function (e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+
+} else {
+    document.body.classList.add('_pc');
+}
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
     });
 
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
-    function toggleMenu() {
-        menu.toggleClass('burger-menu_active');
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
 
-        if (menu.hasClass('burger-menu_active')) {
-            $('body').css('overlow', 'hidden');
-        } else {
-            $('body').css('overlow', 'visible');
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
         }
     }
 }
 
-burgerMenu('.burger-menu');
-// // / burger-menu (jquery) =================
-
+// ============= КОД ДЛЯ БУРГЕР МЕНЮ =============
 
 // smooth scrolling =================
 const anchors = document.querySelectorAll('a[href^="#"]')
@@ -50,9 +109,76 @@ for (let anchor of anchors) {
 // smooth scrolling =================
 
 
+// UP button on the site =================
+let btnUp = $('.up-button'); // создаем переменную для кнопки
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 500) { //отслеживаем высоту от верха страницы в 100px
+        btnUp.addClass('up-button_show'); // присваиваем кнопке класс show для управления видимостью
+    } else {
+        btnUp.removeClass('up-button_show'); // если меньше 500px от верха страницы убираем класс show
+    }
+});
+
+btnUp.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, '500'); //при клике на кнопку плавно прокручиваем до верха body
+});
+// UP button on the site =================
 
 
 
+// JS КОД для АНИМАЦИИ ПРИ СКРОЛЛЕ =============================
+// ============================================================
+
+// находим и объявляем в переменную все объекты которые будут поддаваться анимации
+const animItems = document.querySelectorAll('._anim-items');
+
+// проверяем - существуют ли такие классы 
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    // общая задержка вызова функции 
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
+
+// JS КОД для АНИМАЦИИ ПРИ СКРОЛЛЕ =============================
+// ============================================================
+
+
+
+// ============================================================
 
 
 
--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,183 @@
 // Инициализируем Swiper
 
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+const sliderReviews = document.querySelector('.swiper-reviews-container');
+
+let myImageSliderReviews = new Swiper(sliderReviews, {
+    // Стрелки
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-reviews-navigation__button-next',
+        prevEl: '.swiper-reviews-navigation__button-prev'
     },
+    // Навигация 
+    // Буллеты, текущее положение, прогрессбар
+    pagination: {
+        el: '.swiper-reviews-pagination',
+
+    },
+
+    // Включение/отключение
+    // перетаскивания на ПК
+    simulateTouch: true,
+    // Чувствительность свайпа
+    touchRatio: 1,
+    // Угол срабатывания свайпа/перетаскивания
+    touchAngle: 45,
+    // Курсор перетаскивания
+    grabCursor: true,
+
+    // Переключение при клике на слайд
+    slideToClickedSlide: true,
+
+    // Навигация по хешу
+    hashNavigation: {
+        // Отслеживать состояние
+        watchState: true,
+    },
+
+    // Управление клавиатурой
+    keyboard: {
+        // Включить\выключить
+        enabled: true,
+        // Включить\выключить
+        // только когда слайдер
+        // в пределах вьюпорта
+        onlyInViewport: true,
+        // Включить\выключить
+        // управление клавишами
+        // pageUp, pageDown
+        pageUpDown: false,
+    },
+
+
+    // Автовысота
+    autoHeight: true,
+
+    // Количество слайдов для показа
+    slidesPerView: 1,
+
+    // Отключение функционала
+    // если слайдов меньше чем нужно
+    watchOverflow: false,
+
+    // Отступ между слайдами
+    spaceBetween: 20,
+
+    // Количество пролистываемых слайдов
+    slidesPerGroup: 1,
+
+    // Активный слайд по центру
+    centeredSlides: true,
+
+    // Стартовый слайд.
+    initialSlide: 0,
+
+    // Мультирядность
+    slidesPerColumn: 1,
+
+    // Бесконечный слайдер
+    loop: true,
+
+    // Кол-во дублирующих слайдов
+    loopedSlides: 0,
+
+    // Свободный режим
+    freeMode: false,
+
+    // Скорость
+    speed: 800,
+
+    // Вертикальный слайдер
+    direction: 'horizontal',
+
+    // Эффекты переключения слайдов.
+    // Листание
+    effect: 'slide',
+
+    // Брейк поинты (адаптив)
+    // Ширина экрана
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            // spaceBetween: 10,
+        },
+        576: {
+            slidesPerView: 1.7,
+            spaceBetween: 30,
+            centeredSlides: false,
+        },
+        768: {
+            slidesPerView: 1.7,
+            spaceBetween: 30,
+            centeredSlides: false,
+        },
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            centeredSlides: false,
+        },
+        // 1200: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 30,
+        // },
+        // 1400: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 40,
+        // }
+    },
+
+    /*
+    // Брейк поинты (адаптив)
+    // Соотношение сторон
+    breakpoints: {
+        '@0.75': {
+            slidesPerView: 1,
+        },
+        '@1.00': {
+            slidesPerView: 2,
+        },
+        '@1.50': {
+            slidesPerView: 3,
+        }
+    },
+    */
+
+    // Отключить предзагрузка картинок
+    preloadImages: false,
+    // Lazy Loading
+    // (подгрузка картинок)
+    lazy: {
+        // Подгружать на старте
+        // переключения слайда
+        loadOnTransitionStart: false,
+        // Подгрузить предыдущую
+        // и следующую картинки
+        loadPrevNext: false,
+    },
+    // Слежка за видимыми слайдами
+    watchSlidesProgress: true,
+    // Добавление класса видимым слайдам
+    watchSlidesVisibility: true,
+
+    // Обновить свайпер
+    // при изменении элементов слайдера
+    observer: true,
+
+    // Обновить свайпер
+    // при изменении родительских
+    // элементов слайдера
+    observeParents: true,
+
+    // Обновить свайпер
+    // при изменении дочерних
+    // элементов слайда
+    observeSlideChildren: true,
+
 });
+
+
+
+
+
+
 
 // Инициализируем Swiper
