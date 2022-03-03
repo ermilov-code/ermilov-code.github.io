8ff9230958cd460ecc0da0632a4ee0bc44a80934
paratrooper-94 <ermilov1994@yandex.ru>
2022-03-03 21:01:36 +0300

    new

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img
+      width="200"
+      height="200"
+      alt="Vue logo"
+      src="./assets/calculator.jpeg"
+    />
+    <HelloWorld msg="calculator Vue.js" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#img {
+  max-width: 100%;
+  object-fit: contain;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    {{ msg.length }}
+
+    <hr />
+
+    <div class="display">
+      <input v-model.number="operand1" type="number" />
+      <input v-model.number="operand2" type="number" />
+
+      = {{ result }}
+    </div>
+    <div class="keyboard">
+      <button v-on:click="result = operand1 + operand2">+</button>
+      <button @click="result = operand1 - operand2">-</button>
+      <button @click="divide(operand1, operand2)">/</button>
+      <button @click="multyply">*</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      default: "Empty Props",
+      required: true,
+    },
+  },
+  data() {
+    return {
+      message: "Hello Vue",
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    doThat(str, evnt) {
+      console.log("click", str, evnt);
+    },
+    onValidate() {
+      console.log("validation true");
+    },
+    divide(op1, op2) {
+      this.result = op1 / op2;
+    },
+    multyply() {
+      const { operand1, operand2 } = this;
+      this.result = operand1 * operand2;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  width: 40px;
+  height: 30px;
+  margin: 15px;
+}
+input {
+  padding: 8px;
+  margin: 5px;
+}
+</style>
